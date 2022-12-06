44410dc5c76f26ff37a19a97303d089e4848986f
Macic <46872282+Macic-Dev@users.noreply.github.com>
2022-12-06 19:10:56 +0100

    Use set() for readability

--- day06/day06_1.py
+++ day06/day06_1.py
@@ -4,6 +4,6 @@ for line in fileinput.input():
     line = line.strip()
     if line:
         for i in range(0, len(line)-3):
-            if len({*line[i:i+4]}) == 4:
+            if len(set(line[i:i+4])) == 4:
                 print(i+4)
                 break
--- day06/day06_2.py
+++ day06/day06_2.py
@@ -4,6 +4,6 @@ for line in fileinput.input():
     line = line.strip()
     if line:
         for i in range(0, len(line)-13):
-            if len({*line[i:i+14]}) == 14:
+            if len(set(line[i:i+14])) == 14:
                 print(i+14)
                 break
